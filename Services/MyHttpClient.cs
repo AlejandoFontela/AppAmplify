2faa5c9ee3f19e600f8e0a593401e3af3962763c
AlejandroFontela <ale.fontela.v@gmail.com>
2024-04-07 12:09:47 -0500

    update 2

--- Services/MyHttpClient.cs
+++ Services/MyHttpClient.cs
@@ -1,48 +1,54 @@
 using System.Net.Http;
 using System.Threading.Tasks;
-using Newtonsoft.Json; // Asegúrate de tener instalado el paquete NuGet Newtonsoft.Json
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 namespace BlazorApp.Services
 {
+    // Clases de modelo
     public class MyHttpClient
     {
         private readonly HttpClient _httpClient;
-        private const string apiUrl = "http://ip172-18-0-43-co7lo8a91nsg00e0gg8g-32001.direct.labs.play-with-docker.com/gifstenor";
+        private const string apiUrl = "https://g.tenor.com/v1/search?q=excited&key=LIVDSRZULELA&limit=8";
 
         public MyHttpClient(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
 
-        public async Task<List<GifItem>> GetSomeDataAsync()
+        public async Task<List<MediaItem>> GetMediaItemsAsync()
         {
             var response = await _httpClient.GetAsync(apiUrl);
             response.EnsureSuccessStatusCode();
             var responseData = await response.Content.ReadAsStringAsync();
             var jsonObject = JsonConvert.DeserializeObject<RootObject>(responseData);
-            return jsonObject.Gifs;
+            return jsonObject.Results
+                .SelectMany(r => r.Media)
+                .SelectMany(m => m.Values) // Obtener todas las propiedades bajo 'media'
+                .Select(v => new MediaItem
+                {
+                    Preview = v["preview"]?.ToString(), // Obtener el valor de 'preview'
+                    Url = v["url"]?.ToString() // Obtener el valor de 'url'
+                })
+                .ToList();
         }
-    }
-
-    public class RootObject
-    {
-        public List<GifItem> Gifs { get; set; }
-    }
-
-    public class GifItem
-    {
-        public string Title { get; set; }
-        public List<MediaItem> Media { get; set; }
     }
 
     public class MediaItem
     {
-        public Gif Gif { get; set; }
-    }
-
-    public class Gif
-    {
+        public string Preview { get; set; }
         public string Url { get; set; }
     }
+
+    public class RootObject
+    {
+        public List<ResultItem> Results { get; set; }
+    }
+
+    public class ResultItem
+    {
+        public List<Dictionary<string, JToken>> Media { get; set; } // Cambiar el tipo a Dictionary<string, JToken>
+    }
+
 }
